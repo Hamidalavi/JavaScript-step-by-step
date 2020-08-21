90c14a4fcac3e683be9c21ee4747b9e225d136db
Hamidalavi <ultihamid@gmail.com>
2020-08-22 02:15:30 +0430

    add strings

--- practice.js
+++ practice.js
@@ -5,3 +5,17 @@
 // console.log(hamid); // 23
 
 // -----------------------------------------------
+
+// string
+// let hamid = 'Hello, I am single quotes';
+// console.log(hamid); // "Hello, I am single quotes"
+
+// let hamed = "Hello, I'm double quotes";
+// console.log(hamed); // "Hello, I'm double quotes"
+
+// let ali = `Hello, I'm backticks`;
+// console.log(ali); // "Hello, I'm backticks"
+
+// let value = "modern backticks";
+// let reza = `Hello, I'm ${value}`;
+// console.log(reza); // "Hello, I'm modern backticks"
