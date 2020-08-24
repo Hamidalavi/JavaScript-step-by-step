490c7752cea3285d005211e44759fefae7b69573
Hamidalavi <ultihamid@gmail.com>
2020-08-24 19:32:49 +0430

    built-in functions

---
+++ built-in_functions.js
@@ -0,0 +1,15 @@
+// make sure all text inputs in html are string (even it's type is number) as javaScript detection. make sure dont make mistake
+let variable;
+let array = [];
+
+addEventListener("event", functionname); // add event and we can choose what happen if we click or hover or exit and etc
+parseInt(stringToIntValue); // convert string to int
++variable; // a short hand converting string to int
+parseFloat(toFloatValue); // convert string to float
+variable.toString(); // convert all types (or any types) to string
+array.push(variable); // add new elements (keys) to an array, and returns the new length of the array
+array.length(variable); // gets or sets the length of the array
+array.join(variable); // adds all the elements of an array separated by the specified separator string
+array.forEach(variable); // do specified action for each keys or elements in an array
+
+typeof variable; // show type of that variable
