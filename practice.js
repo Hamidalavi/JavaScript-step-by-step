3d1d5bc5c6dc0ad65e570c64acc2ee4ed0d41c24
Hamidalavi <ultihamid@gmail.com>
2020-09-02 14:30:28 +0430

    add DOM

--- practice.js
+++ practice.js
@@ -1071,19 +1071,23 @@
 
 // ---
 
-const runner = {
-  name: "Runner",
-  run: function (speed) {
-    console.log(`${this.name} runs at ${speed} mph`);
-  },
-};
+// const runner = {
+//   name: "Runner",
+//   run: function (speed) {
+//     console.log(`${this.name} runs at ${speed} mph`);
+//   },
+// };
 
-const flyer = {
-  name: "Flyer",
-  fly: function (speed) {
-    console.log(`${this.name} flies at ${speed} mph`);
-  },
-};
+// const flyer = {
+//   name: "Flyer",
+//   fly: function (speed) {
+//     console.log(`${this.name} flies at ${speed} mph`);
+//   },
+// };
 
-let run = runner.run.bind(flyer, 20);
-run(); // "Flyer runs at 20 mph"
+// let run = runner.run.bind(flyer, 20);
+// run(); // "Flyer runs at 20 mph"
+
+// -----------------------------------------------
+
+// DOM
